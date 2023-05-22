2050fa2edd5e81520a0ff587596550b86062f5ae
James Lee <jameslee@ravpn-200-student-10-8-22-164.uniaccess.unimelb.edu.au>
2023-05-23 04:47:18 +1000

    Added SUDO API

--- Backend/Flask/app.py
+++ Backend/Flask/app.py
@@ -8,6 +8,21 @@ from pysal.explore import esda
 from pysal.lib import weights
 from esda.moran import Moran_Local
 
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from collections import Counter
+from bs4 import BeautifulSoup
+import re
+from datetime import datetime
+from nltk.stem import WordNetLemmatizer
+import string
+import emoji
+
+
+lemmatizer = WordNetLemmatizer()
+stop_words = set(stopwords.words('english'))
+
+
 # authentication
 admin = 'admin'
 password = 'password'
@@ -17,16 +32,20 @@ url = f'http://{admin}:{password}@172.26.131.185:5984/'
 couch = couchdb.Server(url)
 
 # indicate the db name
-db_name = 'twittersegeov3'
+db_name = 'twittersegeov11'
 
 # if not exist, create one
 if db_name not in couch:
     db = couch.create(db_name)
 else:
     db = couch[db_name]
+    
+
+
 
 app = Flask(__name__)
 api = Api(app)
+stop_words = set(stopwords.words('english'))
 #CORS(app, origins=['http://127.0.0.1:8080', 'http://localhost:8081'])
 CORS(app, origins=['*'])
 
@@ -47,6 +66,7 @@ gdf_melbourne.reset_index(drop=True, inplace=True)
 
 # Simplify your dataframe
 simp = gdf_melbourne[["SA2_NAME21", "geometry"]]
+
 
 
 ############
@@ -246,9 +266,9 @@ api.add_resource(SignificantLocalAutocorrelation, '/significant_local_autocorrel
 
 
 class AverageSent(Resource):
-    def get(self, label='LABEL_0'):
+    def get(self, label='positive'):
         # using an existing view
-        view = db.view('exp1/suburbsent', group_level=2, limit=10)
+        view = db.view('exp1/suburbsent', group_level=2)
 
         response = []
 
@@ -261,9 +281,6 @@ class AverageSent(Resource):
                     "value": row.value,
                 })
 
-                # Limit to the first 10 records for debug purpose
-                if len(response) >= 10:
-                    break
             
         return response, 200  # Return with HTTP status 200 (OK)
 
@@ -275,7 +292,7 @@ api.add_resource(AverageSent, '/average_sent', '/average_sent/<label>')
 
 
 class GlobalAutocorrelationSentiment(Resource):
-    def get(self, label='LABEL_0'):
+    def get(self, label='positive'):
         # Get the rule parameter from the request
         rule = request.args.get('rule', 'queen')
 
@@ -315,7 +332,7 @@ api.add_resource(GlobalAutocorrelationSentiment, '/global_autocorrelation_sentim
 
 
 class LocalAutocorrelationSentiment(Resource):
-    def get(self, label='LABEL_0'):
+    def get(self, label='positive'):
         # Get the rule parameter from the request
         rule = request.args.get('rule', 'queen')
 
@@ -366,7 +383,7 @@ api.add_resource(LocalAutocorrelationSentiment, '/local_autocorrelation_sentimen
 
 
 class SignificantLocalAutocorrelationSentiment(Resource):
-    def get(self, label='LABEL_0'):
+    def get(self, label='positive'):
         # Get the significance level parameter from the request
         alpha = float(request.args.get('alpha', 0.05))
 
@@ -409,6 +426,286 @@ class SignificantLocalAutocorrelationSentiment(Resource):
         return {'data': results}
 
 api.add_resource(SignificantLocalAutocorrelationSentiment, '/significant_local_autocorrelation_sentiment', '/significant_local_autocorrelation_sentiment/<label>')
+
+
+
+
+############
+############
+#API for text analysis
+############
+############
+
+
+
+
+class TopWordCounts(Resource):
+    def get(self):
+        start_date = request.args.get('start_date', None)
+        end_date = request.args.get('end_date', None)
+
+        # retrieve all docs
+        view_results = db.view('WordCounts/RawText', include_docs=True, startkey=[start_date, None], endkey=[end_date, {}])
+
+        # get all the tweet texts
+        tweet_texts = [row.value for row in view_results]
+
+        # remove HTML tags
+        tweet_texts = [BeautifulSoup(text, features="lxml").get_text() for text in tweet_texts]
+
+        # remove URLs
+        tweet_texts = [re.sub(r'http\S+|www.\S+', '', text, flags=re.MULTILINE) for text in tweet_texts]
+
+        # remove emojis
+        tweet_texts = [re.sub(':.*?:', '', emoji.demojize(text)) for text in tweet_texts]
+
+        # tokenize and remove stop words, punctuation, and apply lemmatization
+        words = [lemmatizer.lemmatize(word.lower()) for text in tweet_texts for word in word_tokenize(text) if word.lower() not in stop_words and word not in string.punctuation]
+
+        # count occurrences of each word
+        word_counts = Counter(words)
+
+        return {'data': dict(word_counts)}
+
+
+api.add_resource(TopWordCounts, '/top_word_counts')
+
+
+
+class TopWordCountsByAllSuburbs(Resource):
+    def get(self):
+        start_date = request.args.get('start_date', None)
+        end_date = request.args.get('end_date', None)
+
+        # A list of all suburbs
+        suburbs = list(simp["SA2_NAME21"])  # replace this with the list of suburbs you have
+
+        data = {}
+
+        for suburb in suburbs:
+            view_results = db.view('WordCounts/RawText', include_docs=True, startkey=[start_date, suburb], endkey=[end_date, suburb])
+
+            # get all the tweet texts for the specific suburb
+            tweet_texts = [row.value for row in view_results if row.key[1] == suburb]
+
+            # remove HTML tags
+            tweet_texts = [BeautifulSoup(text, features="lxml").get_text() for text in tweet_texts]
+
+            # remove URLs
+            tweet_texts = [re.sub(r'http\S+|www.\S+', '', text, flags=re.MULTILINE) for text in tweet_texts]
+
+            # remove emojis
+            tweet_texts = [re.sub(':.*?:', '', emoji.demojize(text)) for text in tweet_texts]
+
+            # tokenize and remove stop words, punctuation, and apply lemmatization
+            words = [lemmatizer.lemmatize(word.lower()) for text in tweet_texts for word in word_tokenize(text) if word.lower() not in stop_words and word not in string.punctuation]
+
+            # count occurrences of each word
+            word_counts = Counter(words)
+
+            data[suburb] = dict(word_counts)
+
+        return {'data': data}
+
+
+api.add_resource(TopWordCountsByAllSuburbs, '/top_word_counts_by_all_suburbs')
+
+
+
+class TopWordCountsBySuburb(Resource):
+    def get(self):
+        start_date = request.args.get('start_date', None)
+        end_date = request.args.get('end_date', None)
+        suburb = request.args.get('suburb', None)
+
+        # retrieve all docs
+        view_results = db.view('WordCounts/RawText', include_docs=True, startkey=[start_date, suburb], endkey=[end_date, suburb])
+
+        # get all the tweet texts for the specific suburb
+        tweet_texts = [row.value for row in view_results if row.key[1] == suburb]
+
+        # remove HTML tags
+        tweet_texts = [BeautifulSoup(text, features="lxml").get_text() for text in tweet_texts]
+
+        # remove URLs
+        tweet_texts = [re.sub(r'http\S+|www.\S+', '', text, flags=re.MULTILINE) for text in tweet_texts]
+
+        # remove emojis
+        tweet_texts = [re.sub(':.*?:', '', emoji.demojize(text)) for text in tweet_texts]
+
+        # tokenize and remove stop words, punctuation, and apply lemmatization
+        words = [lemmatizer.lemmatize(word.lower()) for text in tweet_texts for word in word_tokenize(text) if word.lower() not in stop_words and word not in string.punctuation]
+
+        # count occurrences of each word
+        word_counts = Counter(words)
+
+        return {'data': dict(word_counts)}
+
+
+api.add_resource(TopWordCountsBySuburb, '/top_word_counts_by_suburb')
+
+
+
+###############
+###############
+#SUDO Data
+###############
+###############
+
+
+db_crime = couch['sudocrime']
+db_rent = couch['sudorent']
+db_population = couch['sudopopulation']
+db_bars = couch['sudobars']
+db_jobsforecasts = couch['sudojobsforecasts']
+db_sudocarpark = couch['sudocarpark']
+
+
+
+class CrimeData(Resource):
+    def get(self):
+        crime_data = {doc["id"]: doc['doc'] for doc in db_crime.view('_all_docs', include_docs=True)}
+        return {'crime_data': crime_data}
+
+
+class RentData(Resource):
+    def get(self):
+        all_docs = {doc['id']: doc['doc'] for doc in db_rent.view('_all_docs', include_docs=True)}
+        rent_data = {}
+        for id, data in all_docs.items():
+            rent_ranges = {
+                ' r_0_74_tot': 74,
+                ' r_75_99_tot': 99,
+                ' r_100_149_tot': 149,
+                ' r_150_199_tot': 199,
+                ' r_200_224_tot': 224,
+                ' r_225_274_tot': 274,
+                ' r_275_349_tot': 349,
+                ' r_350_449_tot': 449,
+                ' r_450_549_tot': 549,
+                ' r_550_649_tot': 649,
+                ' r_650_749_tot': 749,
+                ' r_750_849_tot': 849,
+                ' r_850_949_tot': 949,
+                ' r_950_over_tot': 950  # Consider adjusting this as needed
+            }
+
+            weighted_sum = 0
+            total_rentals = 0
+            for range, max_value in rent_ranges.items():
+                count = data.get(range, 0)
+                weighted_sum += max_value * count
+                total_rentals += count
+
+            if total_rentals != 0:
+                average_rent = weighted_sum / total_rentals
+            else:
+                average_rent = 0
+
+            rent_data[id] = {"SA2_NAME21": data.get("SA2_NAME21"), "average_rent": average_rent}
+
+        return {'rent_data': rent_data}
+
+
+    
+    
+    
+class TotalValueByAge(Resource):
+    def get(self):
+
+        view = db_population.view('sum/geoage', group=True)
+
+        results = {}
+        for row in view:
+            geography, age = row.key
+            total_value = row.value
+            if geography not in results:
+                results[geography] = {}
+            results[geography][age] = total_value
+
+        return {'data': results}
+
+
+    
+class TotalValueByGender(Resource):
+    def get(self):
+
+        view = db_population.view('sum/geogender', group=True)
+
+        results = {}
+        for row in view:
+            geography, gender = row.key
+            total_value = row.value
+            if geography not in results:
+                results[geography] = {}
+            results[geography][gender] = total_value
+
+        return {'data': results}
+
+
+
+class TotalPatronsByArea(Resource):
+    def get(self):
+
+        view = db_bars.view('sum/sum', group=True)
+
+        results = {}
+        for row in view:
+            results[row.key] = row.value
+
+        return {'data': results}
+
+
+
+
+class TotalValueByJobsForecasts(Resource):
+    def get(self):
+
+        view = db_jobsforecasts.view('sum/sum', group=True)
+
+        results = {}
+        for row in view:
+            results[row.key] = row.value
+
+        return {'data': results}
+
+
+    
+    
+    
+    
+class TotalSpacesByAreaAndType(Resource):
+    def get(self):
+        
+        view = db_sudocarpark.view('sum/sum', group=True)
+
+        results = {}
+        for row in view:
+            area, parking_type = row.key
+            total_spaces = row.value
+            if area not in results:
+                results[area] = {}
+            results[area][parking_type] = total_spaces
+
+        return {'data': results}
+
+
+
+
+    
+    
+    
+    
+    
+
+    
+api.add_resource(TotalSpacesByAreaAndType, '/total_spaces_by_area_and_type')
+api.add_resource(TotalValueByJobsForecasts, '/total_value_by_jobsforecasts')
+api.add_resource(TotalPatronsByArea, '/total_patrons_by_area')
+api.add_resource(TotalValueByGender, '/total_value_by_gender')
+api.add_resource(TotalValueByAge, '/total_value_by_age')
+api.add_resource(CrimeData, '/crime_data')
+api.add_resource(RentData, '/rent_data')
 
 
 
